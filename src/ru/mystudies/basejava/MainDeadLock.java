a205fcd45e03c75d487f29c2490b5d7c89428645
Grigoriev_Semen <byblik.sem@yandex.ru>
2022-06-24 22:33:30 +0300

    Made work of mistakes HW11

--- src/ru/mystudies/basejava/MainDeadLock.java
+++ src/ru/mystudies/basejava/MainDeadLock.java
@@ -1,51 +1,31 @@
 package ru.mystudies.basejava;
 
 public class MainDeadLock {
-    public static final Object Lock_1 = new Object();
-    public static final Object Lock_2 = new Object();
+    public static final String Lock_1 = "Lock_1";
+    public static final String Lock_2 = "Lock_2";
 
     public static void main(String[] args) {
-        Thread thread_1 = new Thread(new Test_1(), "Thread 1");
-        Thread thread_2 = new Thread(new Test_2(), "Thread 2");
-
-        thread_1.start();
-        thread_2.start();
+        doRun(Lock_1, Lock_2);
+        doRun(Lock_2, Lock_1);
     }
 
-    private static class Test_1 implements Runnable {
-        public void run() {
-            synchronized (Lock_1) {
-                System.out.println(Thread.currentThread().getName() + " : удерживает блокировку Lock_1");
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    System.out.println(e.getMessage());
-                }
-                System.out.println(Thread.currentThread().getName() + " : ожидает снятие блокировки Lock_2");
-
-                synchronized (Lock_2) {
-                    //do something
-                }
-            }
-        }
-    }
-
-    private static class Test_2 implements Runnable {
-        public void run() {
-            synchronized (Lock_2) {
-                System.out.println(Thread.currentThread().getName() + " : удерживает блокировку Lock_2");
-
-                try {
-                    Thread.sleep(10);
-                } catch (InterruptedException e) {
-                    System.out.println(e.getMessage());
-                }
-                System.out.println(Thread.currentThread().getName() + " : ожидает снятия блокировки Lock_1");
-                synchronized (Lock_1) {
-                    //do something
+    private static void doRun(String str_1, String str_2) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (str_1) {
+                    System.out.println(Thread.currentThread().getName() + " : удерживает блокировку " + str_1);
+                    try {
+                        Thread.sleep(10);
+                        System.out.println(Thread.currentThread().getName() + " : ожидает снятие блокировки c  " + str_2);
+                        synchronized (str_2) {
+                            //do something
+                        }
+                    } catch (InterruptedException e) {
+                        System.out.println(e.getMessage());
+                    }
                 }
             }
-        }
+        }).start();
     }
 }
