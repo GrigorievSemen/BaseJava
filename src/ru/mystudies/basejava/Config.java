d14bbdc14e47409d47f3e59ee4a5971c37345e1a
Grigoriev_Semen <byblik.sem@yandex.ru>
2022-08-21 19:42:36 +0300

    First

--- src/ru/mystudies/basejava/Config.java
+++ src/ru/mystudies/basejava/Config.java
@@ -10,21 +10,22 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-   // private static final File PROPS = new File("C:\\Users\\Sem\\Desktop\\Programming\\JavaProject\\IdeaProjects\\BaseJava\\HW\\config\\resumes.properties");
-    private static final String PROPS = "/resumes.properties";
+   private static final File PROPS = new File("C:\\Users\\Sem\\Desktop\\Programming\\JavaProject\\ReadyProjects\\BaseJava\\HW\\config\\resumes.properties");
+    //private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
     private final File storageDir;
     private final Storage storage;
 
     private Properties props = new Properties();
     private Config() {
-        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
-        //try (InputStream is = new FileInputStream(PROPS)) {
+       // try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
+        try (InputStream is = new FileInputStream(PROPS)) {
             Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
         } catch (IOException | ClassNotFoundException e) {
+            System.out.println(e.getMessage());
             throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
--- src/ru/mystudies/basejava/web/MainSql.java
+++ src/ru/mystudies/basejava/web/MainSql.java
@@ -12,6 +12,8 @@ public class MainSql {
     public static void main(String[] args) {
         Storage storage = Config.get().getStorage();
         Resume RESUME_1 = new Resume(UUID.randomUUID().toString(), "Иванов Пример Примерович");
+        Resume RESUME_2 = new Resume(UUID.randomUUID().toString(), "Сидоров Пример Примерович");
+        Resume RESUME_3 = new Resume(UUID.randomUUID().toString(), "Петров Пример Примерович");
 
         RESUME_1.setContact(ContactType.NUMBER_PHONE, "8(999)999-99-99");
         RESUME_1.setContact(ContactType.SKYPE, "myScype");
@@ -30,7 +32,7 @@ public class MainSql {
         RESUME_1.setSection(SectionType.EXPERIENCE,
                 new OrganizationSection(
                         new Organization("Второе место работы", "",
-                                new Period(DateUtil.of(2005, Month.JANUARY), null, "Руководитель", "Руками вожу.")),
+                                new Period(DateUtil.of(2005, Month.JANUARY), null, "Руководитель", "Описание рабочей деятельности")),
                         new Organization("Первое место работы", "",
                                 new Period(DateUtil.of(2002, Month.MARCH), DateUtil.of(2004, Month.NOVEMBER), "Старший таксист", "Ездил сюда туда"),
                                 new Period(DateUtil.of(2001, Month.FEBRUARY), DateUtil.of(2002, Month.MARCH), "Таксист", "Ездил туда сюда."))));
@@ -39,8 +41,66 @@ public class MainSql {
                 new OrganizationSection(
                         new Organization("Пример учебного заведения", "",
                                 new Period(DateUtil.of(1995, Month.AUGUST), DateUtil.of(2000, Month.MAY), "Получил высшее образование",""),
+
+                                new Period(DateUtil.of(1992, Month.SEPTEMBER), DateUtil.of(1995, Month.MAY), "Получил средние образование", ""))));
+        RESUME_2.setContact(ContactType.NUMBER_PHONE, "8(999)999-99-99");
+        RESUME_2.setContact(ContactType.SKYPE, "myScype");
+        RESUME_2.setContact(ContactType.EMAIL, "sidorov@mmail.ru");
+      //  RESUME_1.setContact(ContactType.PROFILE_LINKEDIN, "https://yandex.ru");
+        RESUME_2.setContact(ContactType.PROFILE_GITHUB, "https://github.com/.....");
+        RESUME_2.setContact(ContactType.PROFILE_STACKOVERFLOW, "https://STACKOVERFLOW");
+       // RESUME_1.setContact(ContactType.HOME_PAGE, "https://yandex.ru");
+
+        RESUME_2.setSection(SectionType.OBJECTIVE, new TextSection("Не унывать!"));
+        RESUME_2.setSection(SectionType.PERSONAL, new TextSection("Пример качество Пример качество Пример качество Пример качество "));
+
+        RESUME_2.setSection(SectionType.ACHIEVEMENT, new ListSection("Пример достижения1", "Пример достижения2", "Пример достижения3", "Пример достижения4\n\n"));
+        RESUME_2.setSection(SectionType.QUALIFICATIONS, new ListSection("Квалификация_1", "Квалификация_2"));
+
+        RESUME_2.setSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Второе место работы", "",
+                                new Period(DateUtil.of(2005, Month.JANUARY), null, "Руководитель", "Описание рабочей деятельности")),
+                        new Organization("Первое место работы", "",
+                                new Period(DateUtil.of(2002, Month.MARCH), DateUtil.of(2004, Month.NOVEMBER), "Старший таксист", "Ездил сюда туда"),
+                                new Period(DateUtil.of(2001, Month.FEBRUARY), DateUtil.of(2002, Month.MARCH), "Таксист", "Ездил туда сюда."))));
+
+        RESUME_2.setSection(SectionType.EDUCATION,
+                new OrganizationSection(
+                        new Organization("Пример учебного заведения", "",
+                                new Period(DateUtil.of(1995, Month.AUGUST), DateUtil.of(2000, Month.MAY), "Получил высшее образование",""),
+
+                                new Period(DateUtil.of(1992, Month.SEPTEMBER), DateUtil.of(1995, Month.MAY), "Получил средние образование", ""))));
+        RESUME_3.setContact(ContactType.NUMBER_PHONE, "8(999)999-99-99");
+        RESUME_3.setContact(ContactType.SKYPE, "myScype");
+        RESUME_3.setContact(ContactType.EMAIL, "petrov@mmail.ru");
+      //  RESUME_1.setContact(ContactType.PROFILE_LINKEDIN, "https://yandex.ru");
+        RESUME_3.setContact(ContactType.PROFILE_GITHUB, "https://github.com/.....");
+        RESUME_3.setContact(ContactType.PROFILE_STACKOVERFLOW, "https://STACKOVERFLOW");
+       // RESUME_1.setContact(ContactType.HOME_PAGE, "https://yandex.ru");
+
+        RESUME_3.setSection(SectionType.OBJECTIVE, new TextSection("Не унывать!"));
+        RESUME_3.setSection(SectionType.PERSONAL, new TextSection("Пример качество Пример качество Пример качество Пример качество "));
+
+        RESUME_3.setSection(SectionType.ACHIEVEMENT, new ListSection("Пример достижения1", "Пример достижения2", "Пример достижения3", "Пример достижения4\n\n"));
+        RESUME_3.setSection(SectionType.QUALIFICATIONS, new ListSection("Квалификация_1", "Квалификация_2"));
+
+        RESUME_3.setSection(SectionType.EXPERIENCE,
+                new OrganizationSection(
+                        new Organization("Второе место работы", "",
+                                new Period(DateUtil.of(2005, Month.JANUARY), null, "Руководитель", "Описание рабочей деятельности")),
+                        new Organization("Первое место работы", "",
+                                new Period(DateUtil.of(2002, Month.MARCH), DateUtil.of(2004, Month.NOVEMBER), "Старший таксист", "Ездил сюда туда"),
+                                new Period(DateUtil.of(2001, Month.FEBRUARY), DateUtil.of(2002, Month.MARCH), "Таксист", "Ездил туда сюда."))));
+
+        RESUME_3.setSection(SectionType.EDUCATION,
+                new OrganizationSection(
+                        new Organization("Пример учебного заведения", "",
+                                new Period(DateUtil.of(1995, Month.AUGUST), DateUtil.of(2000, Month.MAY), "Получил высшее образование",""),
                                 new Period(DateUtil.of(1992, Month.SEPTEMBER), DateUtil.of(1995, Month.MAY), "Получил средние образование", ""))));
 
         storage.save(RESUME_1);
+        storage.save(RESUME_2);
+        storage.save(RESUME_3);
     }
 }
